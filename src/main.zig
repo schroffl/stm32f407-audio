88e5235d9b762f1bd5bcc3ec50f90238f0da5222
schroffl <schroffl@users.noreply.github.com>
2023-12-27 15:12:53 +0100

    Cleanup

--- src/main.zig
+++ src/main.zig
@@ -21,8 +21,7 @@ const wavetable = Wavetable{
 };
 
 var phasor_left = wavetable.phasor(187.5 * 1);
-var phasor_frequency = wavetable.phasor(1);
-// var phasor_right = wavetable.phasor(10);
+var phasor_right = wavetable.phasor(10);
 
 pub const microzig_options = struct {
     pub const interrupts = struct {
@@ -181,133 +180,20 @@ pub fn main() !void {
         try I2C.send(0x4a, &.{ 0x02, 0x9e });
     }
 
-    // const table = Wavetable{
-    //     .samples = [_]f32{0.5} ** 256,
-    // };
-
-    // var scanner = Wavetable.Scanner{ .step_size = 1, .position = 153.79 };
-    // const value0 = table.sample(scanner);
-    // _ = value0;
-    // scanner.step();
-    // const value2 = table.sample(scanner);
-    // _ = value2;
-
     var i2s = I2S.init();
     std.mem.doNotOptimizeAway(i2s);
 
-    //     for (wavetable.table, 0..) |sample, i| {
-    //         if (i > 0) try uart.write_buffer.write(", ");
-    //         try uart.write_buffer.writer().print("{d:.5}", .{sample});
-    //     }
-
     uart.ensureTransmission();
 
-    // Configure the MPU6050
-    // try I2C.send(0x68, &.{ 0x6b, 0x00 });
-    // try I2C.send(0x4a, &.{ 0x00, 0x99 });
-
-    var rand = prng.random();
-    _ = rand;
-
-    while (true) {
-        // const v = wavetable.sample(phasor_left.position);
-        // _ = v;
-        // phasor_left.advance();
-
-        // while (regs.SPI3.SR.read().TXE == 0) {}
-        // regs.SPI3.DR.modify(.{ .DR = rand.int(u16) });
-        // while (regs.SPI3.SR.read().TXE == 0) {}
-        // regs.SPI3.DR.modify(.{ .DR = 0 });
-
-        // top_buffer_half = !top_buffer_half;
-
-        // Toggle an LED for debugging purposes
-        // regs.GPIOD.BSRR.modify(.{
-        //     .BS15 = @as(u1, @bitCast(top_buffer_half)),
-        //     .BR15 = @as(u1, @bitCast(!top_buffer_half)),
-        // });
-    }
-
-    while (true) {
-        // microzig.cpu.nop();
-
-        if (uart.read_buffer.readItem()) |_| {
-            // uart.write_buffer.writeItem(char) catch unreachable;
-            // uart.ensureTransmission();
-
-            var out: [1]u8 = undefined;
-            try I2C.send(0x4a, &.{0x2e | 128});
-            try I2C.read(0x4a, &out);
-            std.log.debug("{b:0<8}", .{out[0]});
-        }
-    }
-
-    const SensorReading = packed struct {
-        x: i16,
-        y: i16,
-        z: i16,
-    };
-
-    while (true) {
-
-        // Tell the MP6050 to dump its contents
-        try I2C.send(0x68, &.{0x3b});
-
-        var out: [@bitSizeOf(SensorReading) / 8]u8 = undefined;
-        try I2C.read(0x68, &out);
-
-        var reading: SensorReading = @bitCast(out);
-        reading.x = @byteSwap(reading.x);
-        reading.y = @byteSwap(reading.y);
-        reading.z = @byteSwap(reading.z);
-
-        const ax = @as(f32, @floatFromInt(reading.x)) / 16384 * 9.806;
-        const ay = @as(f32, @floatFromInt(reading.y)) / 16384 * 9.806;
-        const az = @as(f32, @floatFromInt(reading.z)) / 16384 * 9.806;
-
-        var writer = uart.write_buffer.writer();
-        try writer.print("{d:.5},{d:.5},{d:.5}\n", .{ ax, ay, az });
-        uart.ensureTransmission();
-
-        hackyDelay(250);
-    }
-
-    // try I2C.send(0x94 >> 1, &.{ 0, 0 });
-
-    // try i2cTest();
-
     while (true) microzig.cpu.nop();
-
-    // var idx: usize = 0;
-
-    // while (true) {
-    //     if (uart.read_buffer.readItem()) |char| {
-    //         uart.write_buffer.writeItem(char) catch unreachable;
-    //         uart.ensureTransmission();
-    //     }
-
-    //     if (idx > 60000) {
-    //         // std.log.debug("Yooo", .{});
-    //         idx = 0;
-    //     }
-
-    //     idx += 1;
-
-    //     asm volatile ("nop");
-    // }
 }
 
-var last_sample: f32 = 0.0;
 var counter: usize = 0;
 
 fn fillAudioBuffer(out: []u16) void {
     for (0..out.len / 2) |i| {
-        const freq = phasor_frequency.normalizedPosition() * 100 + 50;
-        _ = freq;
-        phasor_frequency.advance();
-
         const v = wavetable.sample(phasor_left.position);
-        // const v2 = wavetable.sample(phasor_right.position);
+        const v2 = wavetable.sample(phasor_right.position);
 
         if (counter < 256) {
             std.log.debug("{d:.3}", .{phasor_left.position});
@@ -315,125 +201,9 @@ fn fillAudioBuffer(out: []u16) void {
         }
 
         out[i * 2 + 0] = @bitCast(I2S.floatToInt(i16, v));
-        out[i * 2 + 1] = 0;
+        out[i * 2 + 1] = @bitCast(I2S.floatToInt(i16, v2));
 
-        // out[i * 2 + 0] = @bitCast(I2S.floatToInt(i16, v));
-        // out[i * 2 + 1] = @bitCast(I2S.floatToInt(i16, v));
-
-        // if (std.math.fabs(v - last_sample) > 0.3) {
-        //     std.log.debug("Jump from {d:.3} to {d:.3} at index {}", .{ last_sample, v, i });
-        // }
-        // last_sample = v;
-
-        // out[i * 4 + 0] = 0xffff;
-        // out[i * 4 + 1] = 0xffff;
-
-        // out[i * 4 + 2] = @intCast(vint >> 16);
-        // out[i * 4 + 3] = @intCast(vint & 0xffff);
-
-        // out[i * 4 + 0] = 0xdead;
-        // out[i * 4 + 1] = 0xbeef;
-
-        // out[i * 4 + 2] = 0xfaad;
-        // out[i * 4 + 3] = 0xeeda;
-
-        // out[i * 4 + 0] = @intCast(vint >> 16);
-        // out[i * 4 + 1] = @intCast(vint & 0xffff);
-
-        // out[i * 4 + 2] = @intCast(v2int >> 16);
-        // out[i * 4 + 3] = @intCast(v2int & 0xffff);
-
-        // HI Left
-        // out[i * 4 + 2] = @intCast(v2int >> 16);
-        // LO Left
-        // out[i * 4 + 2] = @intCast(v2int & 0xffff);
-
-        // out[i * 4 + 1] = @bitCast(I2S.floatToInt(i16, v));
-
-        // phasor_left.setFrequency(freq);
         phasor_left.advance();
-        // phasor_right.advance();
+        phasor_right.advance();
     }
-}
-
-fn systemInit() void {
-    // This init does these things:
-    // - Enables the FPU coprocessor
-    // - Sets the external oscillator to achieve a clock frequency of 168MHz
-    // - Sets the correct PLL prescalers for that clock frequency
-    // - Enables the flash data and instruction cache and sets the correct latency for 168MHz
-
-    // Enable FPU coprocessor
-    // WARN: currently not supported in qemu, comment if testing it there
-    regs.FPU_CPACR.CPACR.modify(.{ .CP = 0b11 });
-
-    // Enable HSI
-    regs.RCC.CR.modify(.{ .HSION = 1 });
-
-    // Wait for HSI ready
-    while (regs.RCC.CR.read().HSIRDY != 1) {}
-
-    // Select HSI as clock source
-    regs.RCC.CFGR.modify(.{ .SW0 = 0, .SW1 = 0 });
-
-    // Enable external high-speed oscillator (HSE)
-    regs.RCC.CR.modify(.{ .HSEON = 1 });
-
-    // Wait for HSE ready
-    while (regs.RCC.CR.read().HSERDY != 1) {}
-
-    // Set prescalers for 168 MHz: HPRE = 0, PPRE1 = DIV_2, PPRE2 = DIV_4
-    regs.RCC.CFGR.modify(.{ .HPRE = 0, .PPRE1 = 0b101, .PPRE2 = 0b100 });
-
-    // Disable PLL before changing its configuration
-    regs.RCC.CR.modify(.{ .PLLON = 0 });
-
-    // Set PLL prescalers and HSE clock source
-    // TODO: change the svd to expose prescalers as packed numbers instead of single bits
-    regs.RCC.PLLCFGR.modify(.{
-        .PLLSRC = 1,
-        // PLLM = 8 = 0b001000
-        .PLLM0 = 0,
-        .PLLM1 = 0,
-        .PLLM2 = 0,
-        .PLLM3 = 1,
-        .PLLM4 = 0,
-        .PLLM5 = 0,
-        // PLLN = 336 = 0b101010000
-        .PLLN0 = 0,
-        .PLLN1 = 0,
-        .PLLN2 = 0,
-        .PLLN3 = 0,
-        .PLLN4 = 1,
-        .PLLN5 = 0,
-        .PLLN6 = 1,
-        .PLLN7 = 0,
-        .PLLN8 = 1,
-        // PLLP = 2 = 0b10
-        .PLLP0 = 0,
-        .PLLP1 = 1,
-        // PLLQ = 7 = 0b111
-        .PLLQ0 = 1,
-        .PLLQ1 = 1,
-        .PLLQ2 = 1,
-    });
-
-    // Enable PLL
-    regs.RCC.CR.modify(.{ .PLLON = 1 });
-
-    // Wait for PLL ready
-    while (regs.RCC.CR.read().PLLRDY != 1) {}
-
-    // Enable flash data and instruction cache and set flash latency to 5 wait states
-    regs.FLASH.ACR.modify(.{ .DCEN = 1, .ICEN = 1, .LATENCY = 5 });
-
-    // Select PLL as clock source
-    regs.RCC.CFGR.modify(.{ .SW1 = 1, .SW0 = 0 });
-
-    // Wait for PLL selected as clock source
-    var cfgr = regs.RCC.CFGR.read();
-    while (cfgr.SWS1 != 1 and cfgr.SWS0 != 0) : (cfgr = regs.RCC.CFGR.read()) {}
-
-    // Disable HSI
-    regs.RCC.CR.modify(.{ .HSION = 0 });
 }
